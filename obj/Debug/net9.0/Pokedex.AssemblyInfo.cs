c0c4e25db6f6981bb3a684b841a8e74b9051bc02
Ken Ballard <kenballard9@gmail.com>
2025-09-07 12:59:40 -0400

    Added navigation button and removed floating footer

--- obj/Debug/net9.0/Pokedex.AssemblyInfo.cs
+++ obj/Debug/net9.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543224e8c9d7681df702a7f53cd29d1bdb28b8cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
